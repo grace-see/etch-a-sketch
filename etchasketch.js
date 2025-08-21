f8ddc8d7a98eb3f5aae1a906abc6172e89d34bd8
Grace <171896811+gracedespairs@users.noreply.github.com>
2025-08-21 15:56:42 -0500

    Added hover event listeners to squares & implemented drawing feature

--- etchasketch.js
+++ etchasketch.js
@@ -6,6 +6,9 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseover", () => {
+            square.setAttribute("style", "background: gray;");
+        });
         row.appendChild(square);
     }
     container.appendChild(row);
